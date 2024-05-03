ca37a138bd1bedbdc8bb498a802eb96fdbb55beb
Igor Savin <costonied@gmail.com>
2024-05-03 16:29:32 +0300

    Property injection into the code

--- src/test/java/dev/isavin/ExampleResourceTest.java
+++ src/test/java/dev/isavin/ExampleResourceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
 
 @QuarkusTest
 class ExampleResourceTest {
@@ -14,7 +15,7 @@ class ExampleResourceTest {
         .when().get("/hello")
         .then()
         .statusCode(200)
-        .body(is("Hello from Quarkus REST"));
+        .body(containsStringIgnoringCase("hello"));
   }
 
 }
