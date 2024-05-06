95f7e2af15dfc2d803c8654e51f9ff2519ace2f9
Igor Savin <costonied@gmail.com>
2024-05-06 21:58:44 +0300

    Implemented gRPC handler with multiplex streams

--- inventory-service/src/main/java/dev/isavin/inventory/grpc/GrpcInventoryService.java
+++ inventory-service/src/main/java/dev/isavin/inventory/grpc/GrpcInventoryService.java
@@ -4,6 +4,7 @@ import dev.isavin.inventory.database.CarInventory;
 import dev.isavin.inventory.model.*;
 import io.quarkus.grpc.GrpcService;
 import io.quarkus.logging.Log;
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 
@@ -16,21 +17,25 @@ public class GrpcInventoryService implements InventoryService {
   CarInventory inventory;
 
   @Override
-  public Uni<CarResponse> add(InsertCarRequest request) {
-    Car car = new Car();
-    car.setLicensePlateNumber(request.getLicensePlateNumber());
-    car.setManufacturer(request.getManufacturer());
-    car.setModel(request.getModel());
-    car.setId(CarInventory.ids.incrementAndGet());
-    Log.info("Persisting " + car);
-    inventory.getCars().add(car);
+  public Multi<CarResponse> add(Multi<InsertCarRequest> requests) {
 
-    return Uni.createFrom()
-        .item(CarResponse.newBuilder()
-            .setLicensePlateNumber(car.getLicensePlateNumber())
-            .setManufacturer(car.getManufacturer())
-            .setModel(car.getModel())
-            .setId(car.getId()).build());
+    return requests
+        .map(request -> {
+          Car car = new Car();
+          car.setLicensePlateNumber(request.getLicensePlateNumber());
+          car.setManufacturer(request.getManufacturer());
+          car.setModel(request.getModel());
+          car.setId(CarInventory.ids.incrementAndGet());
+          return car;
+        }).onItem().invoke(car -> {
+          Log.info("Persisting " + car);
+          inventory.getCars().add(car);
+        }).map(car -> CarResponse.newBuilder()
+              .setLicensePlateNumber(car.getLicensePlateNumber())
+              .setManufacturer(car.getManufacturer())
+              .setModel(car.getModel())
+              .setId(car.getId()).build());
+
   }
 
   @Override
